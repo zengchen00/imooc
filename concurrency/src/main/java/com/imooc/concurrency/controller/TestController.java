53c99658f55c51736b1805701c6a5aaa04b498f0
zengchen <147429971@qq.com>
2018-12-04 17:43:29 +0800

    '1212'

--- concurrency/src/main/java/com/imooc/concurrency/controller/TestController.java
+++ concurrency/src/main/java/com/imooc/concurrency/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.imooc.concurrency.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
 
     @RequestMapping(value = "test")
